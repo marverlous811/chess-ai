ff41e84aab4eb3d3a18a935c78b0ef37a62a8f13
hieunq <hieunq@ows.vn>
2019-10-15 00:31:33 +0700

    init config

--- public/js/board.js
+++ public/js/board.js
@@ -1,11 +1,48 @@
 class ChessBoard{
-    constructor(){
+    constructor(boardDivID){
         this.config = {
             draggable: true,    
             position: 'start',
+            onMoveEnd: this.onMoveEnd,
+            onDragStart: this.onDragStart,
+            onDrop: this.onDrop,
+            onSnapEnd: this.onSnapEnd,
         }
-        this.board = Chessboard('myBoard', this.config)
+        this.board = Chessboard(boardDivID, this.config)
+        this.game =  new Chess()
+    }
+
+    onMoveEnd = (oldPos, newPos) => {
+        if(this.game.game_over() === true) {
+            console.log("game over")
+        }
+
+        console.log(this.game.fen())
+    }
+
+    onDragStart = (source, piece, position, orientation) => {
+        if(this.game.game_over() === true || piece.search(/^b/) !== -1) {
+            return false
+        }
+    }
+
+    onSnapEnd = () => {
+        this.board.position(this.game.fen());
+    };
+
+    onDrop = (source, target) => {
+        var move = this.game.move({
+            from: source,
+            to: target,
+            promotion: 'q' // NOTE: always promote to a queen for example simplicity
+        });
+
+        // If illegal move, snapback
+        if (move === null) return 'snapback';
+
+        // Log the move
+        console.log(move)
     }
 }
 
-window.board = new ChessBoard()
+window.board = new ChessBoard('myBoard')
